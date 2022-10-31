f3467834c0b6d1381d667605ffb60e6ff9bca4d3
Erik Embervine <vox9000@protonmail.com>
2022-10-31 12:49:56 +0100

    game welcome screen

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -1,5 +1,34 @@
 import random
 
 def start_game():
+    print("""
+                     (
+                    ( )
+                   (   )    
+         \                      /
+          |                    |
+          |THE CAULDRON OF DOOM|
+          |_                 __|
+            |           __  |
+            | _   ___  |  | |
+             U | |   | |  | |
+               | |    U   | | 
+               | |        | |
+               | |         U 
+               | |
+               | |
+                U
+    
+    PREDICT THE DIVINE NUMBER IN THE CAULDRON AND ASCEND.........
+    
+    ELSE..
+    
+    THOU SHALT DESCEND INTO THE DEPTHS OF DESPAIR...MUAHAHAHAHAHA!!!!
+
+    BE FOREWARNED, YOU MAY ONLY GUESS THRICE TIMES!
+    """)
+    print("\nHARK!!! WHICH NUMBERRR HAVE I CONJURRRED IN THE CAULDRON??!!")
+    answer = random.randrange(1000)
+
 
 start_game()
